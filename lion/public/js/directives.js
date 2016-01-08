46e98f054a67ba185e11f4cb26a51b6751363b79
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2016-01-08 16:10:59 -0500

    added is_liked function to determine whether or not a picture's id already exists in localStorage

--- lion/public/js/directives.js
+++ lion/public/js/directives.js
@@ -9,25 +9,45 @@ angular.module("Lion.directives",[])
     link:function(scope,el,attrs){
 
     },
-    controller:function($scope,socket,NotifService){
-      $scope.liked = false;
+    controller:function($scope,socket,NotifService,$localStorage){
+      $scope.store = $localStorage;
+      $scope.store.likes = $localStorage.likes || [];
+
       var notif_cb = function(err,resp){
         err?console.log(err):console.log(resp);
       }
 
-      $scope.like_toggle = function(pic){
+      $scope.is_liked = function(id)
+      {
+          $scope.liked = $scope.store.likes.indexOf(id) > -1 ? true:false;
+          return $scope.liked;
+      }
+
+      $scope.save_like = function(id){
+        console.log(id);
+        $scope.store.likes.push(id);
+      }
+
+      $scope.remove_like = function($index){
+          $scope.store.likes.splice($index,1);
+      }
+
+      $scope.like_toggle = function(pic,index){
+        //TODO: should not increment or decrement like until response is received from NotifService
         if(!$scope.liked)
         {
           $scope.liked = true;
           pic.likes = pic.likes + 1;
           socket.emit("like",{"pic":pic});
           NotifService.like(pic._id,notif_cb);
+          $scope.save_like(pic._id);
         }
         else {
           $scope.liked = false;
           pic.likes = pic.likes - 1;
           socket.emit("unlike",{"pic":pic});
           NotifService.unlike(pic._id,notif_cb);
+          $scope.remove_like(index);
         }
       }
 
