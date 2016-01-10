3f824a8b9b519aaa449af6b7eaf96c47f3044586
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2016-01-10 17:04:45 -0500

    Should have used the index of the id in localStorage instead of the ngRepeat id of the item

--- lion/public/js/directives.js
+++ lion/public/js/directives.js
@@ -28,26 +28,30 @@ angular.module("Lion.directives",[])
         $scope.store.likes.push(id);
       }
 
-      $scope.remove_like = function($index){
-          $scope.store.likes.splice($index,1);
+      $scope.remove_like = function(id){
+          var idx = $scope.store.likes.indexOf(id);
+          $scope.store.likes.splice(idx,1);
       }
 
       $scope.like_toggle = function(pic,index){
         //TODO: should not increment or decrement like until response is received from NotifService
         if(!$scope.liked)
         {
-          $scope.liked = true;
-          pic.likes = pic.likes + 1;
-          socket.emit("like",{"pic":pic});
-          NotifService.like(pic._id,notif_cb);
-          $scope.save_like(pic._id);
+          if(NotifService.like(pic._id,notif_cb)){
+            $scope.liked = true;
+            pic.likes = pic.likes + 1;
+            socket.emit("like",{"pic":pic});
+            $scope.save_like(pic._id);
+          }
         }
         else {
-          $scope.liked = false;
-          pic.likes = pic.likes - 1;
-          socket.emit("unlike",{"pic":pic});
-          NotifService.unlike(pic._id,notif_cb);
-          $scope.remove_like(index);
+          if(NotifService.unlike(pic._id,notif_cb))
+          {
+            $scope.liked = false;
+            pic.likes = pic.likes - 1;
+            socket.emit("unlike",{"pic":pic});
+            $scope.remove_like(pic._id);
+          }
         }
       }
 
