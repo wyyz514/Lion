abf3f9cd9985fbeeb6cd912d2cbe128af7e4142a
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2016-01-10 17:40:27 -0500

    Added logic to hide/show spinner based on whether pictures are available

--- lion/public/js/controllers.js
+++ lion/public/js/controllers.js
@@ -1,4 +1,8 @@
 angular.module("Lion.controllers",[])
 .controller("AppCtrl",function($scope,pix){
+  $scope.loaded = false;
   $scope.pix = pix.data;
+  $scope.$watch("pix",function(){
+    $scope.loaded = true;
+  });
 });
