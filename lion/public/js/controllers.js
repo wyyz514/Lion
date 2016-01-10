b21a19cc7ae668a67d48613fafd566945eff3076
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2016-01-10 18:28:54 -0500

    changed styling to accomodate notification bar

--- lion/public/js/controllers.js
+++ lion/public/js/controllers.js
@@ -1,5 +1,5 @@
 angular.module("Lion.controllers",[])
-.controller("AppCtrl",function($scope,pix){
+.controller("AppCtrl",function($scope,pix,$timeout){
   $scope.loaded = false;
   $scope.pix = pix.data;
   $scope.$watch("pix",function(){
